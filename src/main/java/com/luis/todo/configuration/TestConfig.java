aa267778de881a41fb60cb4a7c623a320f756dc6
Luis Felipe <1luisfelipe2@gmail.com>
2022-06-03 14:51:44 -0400

    removed security dependency

--- src/main/java/com/luis/todo/configuration/TestConfig.java
+++ src/main/java/com/luis/todo/configuration/TestConfig.java
@@ -7,8 +7,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
