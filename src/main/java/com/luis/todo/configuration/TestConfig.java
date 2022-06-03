ef98aa8e74d65e71ac53f5345edb01be8bd6bce9
Luis Felipe <1luisfelipe2@gmail.com>
2022-06-03 14:42:40 -0400

    removed cors

--- src/main/java/com/luis/todo/configuration/TestConfig.java
+++ src/main/java/com/luis/todo/configuration/TestConfig.java
@@ -6,19 +6,26 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 import com.luis.todo.services.DBService;
 
 @Configuration
 @Profile("test")
-public class TestConfig {
+public class TestConfig extends WebSecurityConfigurerAdapter{
 
-		@Autowired
-		private DBService dbService;
-		
-		@Bean
-		public boolean instancia() throws ParseException {
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.cors().and().csrf().disable();
+	}
+
+	@Autowired
+	private DBService dbService;
+
+	@Bean
+	public boolean instancia() throws ParseException {
 		this.dbService.instanciaBaseDeDados();
-			return true;
-		}
+		return true;
+	}
 }
