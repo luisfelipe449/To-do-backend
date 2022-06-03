996be2738c95d7f79054aa59d2acf33be76f6d16
Luis Felipe <1luisfelipe2@gmail.com>
2022-06-03 14:47:16 -0400

    disabled cors

--- src/main/java/com/luis/todo/configuration/TestConfig.java
+++ src/main/java/com/luis/todo/configuration/TestConfig.java
@@ -1,6 +1,7 @@
 package com.luis.todo.configuration;
 
 import java.text.ParseException;
+import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
@@ -8,16 +9,23 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import com.luis.todo.services.DBService;
 
 @Configuration
 @Profile("test")
-public class TestConfig extends WebSecurityConfigurerAdapter{
+public class TestConfig extends WebSecurityConfigurerAdapter {
 
-	@Override
-	protected void configure(HttpSecurity http) throws Exception {
-		http.cors().and().csrf().disable();
+	@Bean
+	CorsConfigurationSource corsConfigurationSource() {
+		CorsConfiguration configuration = new CorsConfiguration().applyPermitDefaultValues();
+		configuration.setAllowedMethods(Arrays.asList("POST", "GET", "PUT", "DELETE", "OPTIONS"));
+		final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		source.registerCorsConfiguration("/**", configuration);
+		return source;
 	}
 
 	@Autowired
